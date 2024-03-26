0796d7359f39c0509852317633bd4d9a35e60614
Czovek Balazs <czovek.balazs41@gmail.com>
2024-03-26 10:18:18 +0100

    app.js developement, file replacement

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 var emps = [
-    {
-        id: 1,
-        name: "Erős István",
-        city: "Szeged"
-    },
-    {
-        id: 2,
-        name: "Pala Irén",
-        city: "Hatvan"
-    }
-]
-console.log(emps.employees[0].name)
+        {
+            id: 1,
+            name: "Erős István",
+            city: "Szeged"
+        },
+        {
+            id: 2,
+            name: "Pala Irén",
+            city: "Hatvan"
+        }
+    ]
+
+const tbody = document.querySelector('#tbody');
+var rows ='';
+emps.forEach( (emp) => {
+    var row = `
+    <tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.city}</td>
+    </tr>
+    `;
+    rows = rows + row;
+})
+tbody.innerHTML = rows;
