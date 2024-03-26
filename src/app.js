c03a1b463c52f0e57ca3e2bec1ee7a940aa0355b
Czovek_Balazs <czovek.balazs41@gmail.com>
2024-03-26 09:00:16 +0100

    kezdés

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var emps = [
+    {
+        name: "Erős István",
+        city: "Szeged"
+    },
+    {
+        name: "Pala Irén",
+        city: "Hatvan"
+    }
+]
+console.log(emps.employees[0].name)
